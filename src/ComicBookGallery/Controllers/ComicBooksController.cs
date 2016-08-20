b5db996d947067e97f7074d650838492ce8235c8
Glenn Seplowitz <fredzachrcool@optonline.net>
2016-08-19 20:16:12 -0400

    Added comic book repository.

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -11,23 +11,8 @@ namespace ComicBookGallery.Controllers
     public class ComicBooksController : Controller
     {
         public ActionResult Detail()
-        {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() {Name = "Script", Role = "Dan Slott" },
-                    new Artist() {Name = "Penceils", Role = "Humerto Ramos" },
-                    new Artist() {Name = "Inks", Role = "Victor Olazaba" },
-                    new Artist() {Name = "Colors", Role = "Edgar Delgado" },
-                    new Artist() {Name = "Letters", Role = "Chris Eliopoulos" },
-                }
-            };
-            
-            return View(comicBook);
+        {            
+            return View();
         }
     }
 }
